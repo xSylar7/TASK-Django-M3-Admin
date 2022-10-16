6b6aa7d764d9a4ae97ceff95fd89ee67f82c220c
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-16 10:21:27 +0300

    fixed modles.py and added admin.py

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,19 @@
+from unicodedata import name
 from django.contrib import admin
-
+from .models import Pokemon
 # Register your models here.
+
+
+@admin.register(Pokemon)
+class PokemonAdmin (admin.ModelAdmin):
+    list_display = ("id", "name", "hp", "active",)
+    list_filter = ("active",)
+    readonly_fields = ("created_at", "updated_at",)
+    list_display_links = ("id", "name",)
+
+    fieldsets = (
+        ("Main View", {"fields": ("name", "hp", "active", "type")}),
+        ("Localizations", {"fields": ("name_ar", "name_fr", "name_jp",),
+                           "classes": ("collapse",)}),
+        (None, {"fields": ("created_at", "updated_at")})
+    )
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,22 +1,26 @@
 from email.policy import default
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+
 class Pokemon (models.Model):
     class PokemonType(models.TextChoices):
-        WATER="WA"
-        GRASS="GR"
-        GHOST="GH"
-        STEEL="ST"
-        FAIRY="FA"
-
+        WATER = "WA"
+        GRASS = "GR"
+        GHOST = "GH"
+        STEEL = "ST"
+        FAIRY = "FA"
 
     name = models.CharField(max_length=30)
-    type = models.CharField(max_length=30, choices=PokemonType.choices)
-    hp = models.PositiveIntegerField()
+    type = models.CharField(max_length=2, choices=PokemonType.choices)
+    hp = models.PositiveIntegerField(validators=[MinValueValidator(50, "cant be below 50"),
+                                                 MaxValueValidator(350, "can't be a above 350")])
     active = models.BooleanField(True)
-    name_fr = models.CharField(max_length=30, default="", blank = True)
-    name_ar = models.CharField(max_length=30, default="", blank = True)
-    name_jp = models.CharField(max_length=30, default="", blank = True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    modified_at = models.DateTimeField(auto_now = True)
+    name_fr = models.CharField(max_length=30, default="", blank=True)
+    name_ar = models.CharField(max_length=30, default="", blank=True)
+    name_jp = models.CharField(max_length=30, default="", blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
